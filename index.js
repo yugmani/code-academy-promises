30af351a4e747558604c707f96d1b13aa8bc229f
yugmani <yugmani@yahoo.com>
2022-04-20 23:28:26 -0700

    promise or orderSunglass

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 // Import stylesheets
 import './style.css';
 
-//
+//Constructing a promise object
+
+const executorFunction = (resolve, reject) => {
+  if (someCondition) {
+    resolve('I resolved.');
+  } else {
+    reject('I rejected');
+  }
+};
+
+const myFirstPromise = new Promise(executorFunction);
+console.log(myFirstPromise);
+
+//Tasks
+let myExecutor = (resolve, reject) => {
+  if (inventory.sunglasses > 0) {
+    resolve('Sunglasses order processed.');
+  } else {
+    reject('That item is sold out.');
+  }
+};
+
+const orderSunglasses = () => new Promise(myExecutor);
+
+const orderPromise = orderSunglasses();
+console.log(orderPromise);
